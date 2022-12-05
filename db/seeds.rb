05c5e594c6365a6a5d0df1eec1cfd9cd6fea3dae
Keila Lopez <keilalofra@gmail.com>
2022-12-05 15:45:10 -0600

    Seed file started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-puts "🌱 Seeding spices..."
+Customer.destroy_all
+Item.destroy_all
+Order.destroy_all
 
-# Seed your database here
-
-puts "✅ Done seeding!"
+c1 = Customer.create(username: "jpal15", name: "Jeremy Pal" address: "310 Central Ave Waterloo TN 35677 USA", phone: 6741106893)
+c2 = Customer.create(username: "alina3", name: "Alina Taylor" address: "188 E 54th ST Brooklyn NY 11203 USA", phone: 7739306569)
+c3 = Customer.create(username: "bb55", name: "Brandon Brown" address: "1620 97th St Tacoma WA 98444 USA", phone: 3233545117)
