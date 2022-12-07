27c74c71392bb5aff7f507dd089500c2f1b7d3fb
Martin <souzam@sonic.net>
2022-12-07 16:29:50 -0500

    Added DELETE endpoint for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,4 +10,14 @@ class OrdersController < ApplicationController
     get '/orders' do
         Order.all.to_json
     end
+
+    patch '/orders/:id' do
+        order = Order.find(params[:id])
+        order.update(quantity: params[:quantity])
+        order.to_json
+    end
+
+    delete '/orders/:id' do
+        Order.find(params[:id]).destroy
+    end
 end
